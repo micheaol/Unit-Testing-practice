557dc5bf2c34292790580133d0ab80a9215ca757
micheaol <micheaol80@gmail.com>
2021-11-03 00:34:09 +0100

    Task 2: reverseString function

--- function.test.js
+++ function.test.js
@@ -2,5 +2,9 @@ const functions = require('./functions');
 
 
 test('String length should be greater than 1 but not more than 10', () => {
-    expect(functions.stringLength('iiiiiiiiii')).toBeLessThanOrEqual(10);
+    expect(functions.stringLength('i')).toBeLessThanOrEqual(10);
+})
+
+test('cake to ekac', () => {
+    expect(functions.reverseString('cake')).toBe('ekac');
 })
--- functions.js
+++ functions.js
@@ -4,6 +4,13 @@ const functions = {
           if (stringCount > 0 && stringCount <= 10) {
             return stringCount;
           } 
+    },
+    reverseString: (string) =>{
+        let newStr = "";
+        for(let i = string.length - 1; i >= 0; i--){
+            newStr += string[i];
+        }
+        return newStr;
     }
 }
 
