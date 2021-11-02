cf509afd201af5df5e3243f0efa99bf462d1d6c9
micheaol <micheaol80@gmail.com>
2021-11-03 00:44:10 +0100

    add calculator class to the file

---
+++ calculator.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor(a, b){
+        this.a = a;
+        this.b = b;
+    }
+
+    add(a, b){
+      return a + b
+    }
+
+    divide(a, b){
+      return a/b
+    }
+}
